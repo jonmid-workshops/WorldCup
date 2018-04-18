1c9dcd940d0802783367520e68b124a332767cd4
JonMid <jamideros@hotmail.com>
2018-04-18 17:46:07 -0500

    Bottom Navigation

--- app/src/main/java/com/example/jonmid/worldcup/Views/HomeActivity.java
+++ app/src/main/java/com/example/jonmid/worldcup/Views/HomeActivity.java
@@ -1,15 +1,22 @@
 package com.example.jonmid.worldcup.Views;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.jonmid.worldcup.R;
+import com.example.jonmid.worldcup.Utilities.BottomNavigationViewHelper;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.id_bnv_index);
+        BottomNavigationViewHelper.removeShiftMode(bottomNavigationView);
     }
 }
