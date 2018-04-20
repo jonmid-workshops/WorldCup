216c1559c254dfd35f43fbed9de91aca193761c9
JonMid <jamideros@hotmail.com>
2018-04-20 14:51:03 -0500

    Programacion

--- app/src/main/java/com/example/jonmid/worldcup/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/jonmid/worldcup/Fragments/ProfileFragment.java
@@ -21,10 +21,12 @@ public class ProfileFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        // return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
 }
--- app/src/main/java/com/example/jonmid/worldcup/Fragments/StadiumFragment.java
+++ app/src/main/java/com/example/jonmid/worldcup/Fragments/StadiumFragment.java
@@ -3,9 +3,11 @@ package com.example.jonmid.worldcup.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.jonmid.worldcup.R;
 
@@ -14,6 +16,8 @@ import com.example.jonmid.worldcup.R;
  */
 public class StadiumFragment extends Fragment {
 
+    CardView cardViewNewsUno;
+
 
     public StadiumFragment() {
         // Required empty public constructor
@@ -21,10 +25,22 @@ public class StadiumFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_stadium, container, false);
+
+        cardViewNewsUno = (CardView) view.findViewById(R.id.id_cv_news_uno);
+
+        cardViewNewsUno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(), "dfgsdfg", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stadium, container, false);
+        //return inflater.inflate(R.layout.fragment_stadium, container, false);
     }
 
 }
